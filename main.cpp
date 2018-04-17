1700636ac091365bee98f55f5f774bf32fcde313
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-04-17 11:40:09 -0700

    done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+
 using namespace std;
+
+void swapValue (int&, int&, int&);
 
 int main()
 {
@@ -10,7 +13,7 @@ int main()
   //...END OF "DO NOT CHANGE" AREA
 
 
-
+  swapValue(red,green,blue);
 
 
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -18,4 +21,33 @@ int main()
   cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
   return 0;
   //...END OF "DO NOT CHANGE" AREA
+}
+
+void swapValue ( int& red, int& green, int& blue)
+{
+  int tempR = 0;
+  int tempB = 0;
+  int tempG = 0;
+  
+  tempR = red;
+  tempG = green;
+  tempB = blue;
+
+  if ( red < green )
+  {
+    red = green;
+    green = tempR;
+  }
+  if ( red < blue )
+  {
+    blue = red;
+    red = tempB;
+    
+  }
+  if ( green < blue )
+  {
+    tempG = green;
+    green = blue;
+    blue = tempG;
+  }
 }
